3947cbadf50dd74e3be1ac19b8312913fd33d455
Niels Hofmans <niels.hofmans@kbc.be>
2019-09-04 08:25:23 +0200

    fix(variables): fix vars + remove expired token

--- variables.tf
+++ variables.tf
@@ -1,24 +1,18 @@
 variable "scaleway_token" {
-    default = "136f5287-2084-4db5-a1ea-2e50edcd108c"
 }
 
 variable "scaleway_org" {
-    default = "7e4b03e9-8d86-4a69-8001-309889fffbeb"
 }
 
 variable "ssh_private_key_path" {
-    default = "/Users/niels/.ssh/keys/scaleway"
 }
 
 variable "ssh_public_key_path" {
-    default = "/Users/niels/.ssh/keys/scaleway.pub"
 }
 
 variable "ssh_tech_public_key_path" {
-    default = "/Users/niels/.ssh/keys/scaleway.pub"
 }
 
-/*
 variable "cloudflare_email" {
 
 }
@@ -30,4 +24,3 @@ variable "cloudflare_token" {
 variable "github_token" {
     
 }
-*/
