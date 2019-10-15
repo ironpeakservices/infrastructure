e919cefd980b5f78131f54b82cb876234e7dcae1
Niels Hofmans <niels.hofmans@kbc.be>
2019-10-15 07:36:02 +0200

    refactor(secgroup): heavily simplify config

--- modules/node/security-groups.tf
+++ modules/node/security-groups.tf
@@ -1,3 +1,8 @@
+locals {
+  tcpPorts = ["${var.ssh_port}", 80, 433, 2377, 7946]
+  udpPorts = [7946, 4789]
+}
+
 resource "scaleway_security_group" "swarm_instance" {
   name        = "swarm_instances"
   description = "Allow SSH, HTTP(S) and internal Swarm traffic"
@@ -5,74 +10,22 @@ resource "scaleway_security_group" "swarm_instance" {
   inbound_default_policy  = "drop"
   outbound_default_policy = "accept"
   stateful                = true
-}
 
-resource "scaleway_security_group_rule" "ssh_accept" {
-  security_group = "${scaleway_security_group.swarm_instance.id}"
+  dynamic "inbound_rule" {
+    for_each = local.tcpPorts
+    content {
+      action    = "accept"
+      port      = inbound_rule.value
+      protocol  = "TCP"
+    }
+  }
 
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "0.0.0.0/0"
-  protocol  = "TCP"
-  port      = "${var.ssh_port}"
-}
-
-resource "scaleway_security_group_rule" "http_accept" {
-  security_group = "${scaleway_security_group.swarm_instance.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "0.0.0.0/0"
-  protocol  = "TCP"
-  port      = 80
-}
-
-resource "scaleway_security_group_rule" "https_accept" {
-  security_group = "${scaleway_security_group.swarm_instance.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "0.0.0.0/0"
-  protocol  = "TCP"
-  port      = 443
-}
-
-resource "scaleway_security_group_rule" "swarm_tcp_2377_accept" {
-  security_group = "${scaleway_security_group.swarm_instance.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "${var.private_subnet}"
-  protocol  = "TCP"
-  port      = 2377
-}
-
-resource "scaleway_security_group_rule" "swarm_tcp_7946_accept" {
-  security_group = "${scaleway_security_group.swarm_instance.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "${var.private_subnet}"
-  protocol  = "TCP"
-  port      = 7946
-}
-
-resource "scaleway_security_group_rule" "swarm_udp_7946_accept" {
-  security_group = "${scaleway_security_group.swarm_instance.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "${var.private_subnet}"
-  protocol  = "UDP"
-  port      = 7946
-}
-
-resource "scaleway_security_group_rule" "swarm_udp_4789_accept" {
-  security_group = "${scaleway_security_group.swarm_instance.id}"
-
-  action    = "accept"
-  direction = "inbound"
-  ip_range  = "${var.private_subnet}"
-  protocol  = "UDP"
-  port      = 4789
+  dynamic "inbound_rule" {
+    for_each = local.udpPorts
+    content {
+      action    = "accept"
+      port      = inbound_rule.value
+      protocol  = "UDP"
+    }
+  }
 }
