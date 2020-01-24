9954ec024595b0a4dc8cc2b12a917b4b767905e2
Niels Hofmans <hello@ironpeak.be>
2020-01-24 15:19:35 +0100

    fix(k8s): fix annotation for secret

--- modules/kubernetes/accounts.tf
+++ modules/kubernetes/accounts.tf
@@ -13,6 +13,9 @@ resource "kubernetes_secret" "sa-github-mainwebsite-deployer" {
     metadata {
         namespace = kubernetes_namespace.mainwebsite.metadata.0.name
         name = "sa-github-mainwebsite-deployer"
+        annotations = {
+          "kubernetes.io/service-account.name" = "${kubernetes_service_account.github-mainwebsite-deployer.metadata.0.name}"
+        }
     }
 
     /*
