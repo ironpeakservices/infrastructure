d13e0d7059058b40b39a5fe0e1c11958db331d4c
Niels Hofmans <hello@ironpeak.be>
2020-01-24 15:21:36 +0100

    fix(k8s): remove ref cycle

--- modules/kubernetes/accounts.tf
+++ modules/kubernetes/accounts.tf
@@ -3,10 +3,6 @@ resource "kubernetes_service_account" "github-mainwebsite-deployer" {
         namespace = kubernetes_namespace.mainwebsite.metadata.0.name
         name = "github-mainwebsite-deployer"
     }
-
-    secret {
-        name = kubernetes_secret.sa-github-mainwebsite-deployer.metadata.0.name
-    }
 }
 
 resource "kubernetes_secret" "sa-github-mainwebsite-deployer" {
