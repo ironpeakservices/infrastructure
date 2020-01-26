7198e944e14572625971ce387c1d1bafb2b2b7cb
Niels Hofmans <hello@ironpeak.be>
2020-01-26 17:03:18 +0100

    work

--- variables.tf
+++ variables.tf
@@ -4,14 +4,16 @@ variable "scaleway_secrettoken" {}
 
 variable "scaleway_org" {}
 
+variable "cloudflare_token" {}
+
+/*
 variable "ssh_root_private_key" {}
 
 variable "ssh_root_public_key" {}
 
 variable "ssh_tech_public_key" {}
 
-variable "cloudflare_token" {}
-
 variable "github_token" {}
 
 variable "github_org" {}
+*/
