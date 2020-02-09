39e8db5870c22e9cbb3e79a13a7ab911b21bfc29
Niels Hofmans <hello@ironpeak.be>
2020-02-09 15:11:15 +0100

    try kubeconfig

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,5 +1,6 @@
 provider "kubernetes" {
-    load_config_file = false
+    load_config_file = true
+    config_path = "kubeconfig"
     host  = var.host
     token  = var.token
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
