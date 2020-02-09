2750399bce3649957d33b58f9c7a8cd805e668a4
Niels Hofmans <hello@ironpeak.be>
2020-02-09 13:35:15 +0100

    revert to [0]

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-    load_config_file = true
+    load_config_file = false
     host  = var.host
     token  = var.token
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
