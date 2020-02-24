6ed166717c98ca2561f73a6e1cf063ef67be5e18
Niels Hofmans <hello@ironpeak.be>
2020-02-24 12:15:23 +0100

    feat(k8s): add default limits

---
+++ modules/kubernetes/limits.tf
@@ -0,0 +1,30 @@
+resource "kubernetes_limit_range" "default-resource-limitations" {
+  metadata {
+    name = "default-resource-limitations"
+  }
+
+  spec {
+    limit {
+      type = "Pod"
+      max = {
+        cpu    = "200m"
+        memory = "2048M"
+      }
+    }
+
+    limit {
+      type = "PersistentVolumeClaim"
+      min = {
+        storage = "1000M"
+      }
+    }
+
+    limit {
+      type = "Container"
+      default = {
+        cpu    = "50m"
+        memory = "500M"
+      }
+    }
+  }
+}
