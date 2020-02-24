b91e3102dde8cf7be4866f64e1ec6c106206b21f
Niels Hofmans <hello@ironpeak.be>
2020-02-24 14:36:45 +0100

    temp disable k8s

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ module "scaleway" {
     scaleway_organization   = var.scaleway_org
 }
 
-module "kubernetes" {
-    source                  = "./modules/kubernetes"
-
-    host                    = module.scaleway.kubeconfig[0].host
-    token                   = module.scaleway.kubeconfig[0].token
-    cluster_ca_certificate  = module.scaleway.kubeconfig[0].cluster_ca_certificate
-}
-
 module "cloudflare" {
     source                  = "./modules/cloudflare"
     
@@ -21,6 +13,14 @@ module "cloudflare" {
 }
 
 /*
+module "kubernetes" {
+    source                  = "./modules/kubernetes"
+
+    host                    = module.scaleway.kubeconfig[0].host
+    token                   = module.scaleway.kubeconfig[0].token
+    cluster_ca_certificate  = module.scaleway.kubeconfig[0].cluster_ca_certificate
+}
+        
 module "github" {
     source                  = "./modules/github"
 
