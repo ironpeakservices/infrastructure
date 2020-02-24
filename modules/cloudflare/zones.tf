928d4c786bdcf82dbe724f01bd0e634d7c7f87b3
Niels Hofmans <hello@ironpeak.be>
2020-02-24 11:29:27 +0100

    fix(cloudflare): set www proxied

--- modules/cloudflare/zones.tf
+++ modules/cloudflare/zones.tf
@@ -1,13 +1,13 @@
 resource "cloudflare_zone" "ironpeakbe" {
   paused = false
-#  plan   = "free"
+  plan   = "free"
   type   = "full"
   zone   = "ironpeak.be"
 }
 
 resource "cloudflare_zone" "ironpeakservices" {
   paused = false
-#  plan   = "free"
+  plan   = "free"
   type   = "full"
   zone   = "ironpeak.services"
 }
