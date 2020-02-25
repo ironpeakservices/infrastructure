cddaed0c051e74740f11ad0d2a0a4464afe7465e
Niels Hofmans <hello@ironpeak.be>
2020-02-25 12:17:56 +0100

    fix(helm): add istio-init

--- modules/helm/istio.tf
+++ modules/helm/istio.tf
@@ -1,8 +1,24 @@
+# repo of the istio helm charts
 data "helm_repository" "istio" {
   name = "istio.io"
   url  = "https://storage.googleapis.com/istio-release/releases/${var.istio_version}/charts/"
 }
 
+# initialize our istio Custom Resource Defitinations
+# these are needed for the real istio charts
+resource "helm_release" "istio-init" {
+  name        = "istio-init"
+  repository  = data.helm_repository.istio.metadata[0].name
+  chart       = "istio.io/istio-init"
+  version     = var.istio_version
+  namespace   = var.istio_namespace
+  
+  cleanup_on_fail = true
+  recreate_pods   = true
+  verify          = true
+}
+
+# istio chart for the real components
 resource "helm_release" "istio" {
   name        = "istio"
   repository  = data.helm_repository.istio.metadata[0].name
