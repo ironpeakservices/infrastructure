95e2f2c241947fcdf0df4cc7164db0f7256f3a81
Niels Hofmans <hello@ironpeak.be>
2020-02-25 10:23:22 +0100

    fix(helm): add istio repo

--- modules/helm/istio.tf
+++ modules/helm/istio.tf
@@ -4,6 +4,7 @@ data "helm_repository" "istio" {
 }
 
 resource "helm_release" "istio" {
-  name  = "istio"
-  chart = "istio.io/istio"
+  name        = "istio"
+  repository  = data.helm_repository.istio.metadata[0].name
+  chart       = "istio.io/istio"
 }
