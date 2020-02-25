50290b56cb9fb2bda62b2fd9545ac2ee0f574efb
Niels Hofmans <hello@ironpeak.be>
2020-02-25 10:43:46 +0100

    fix(helm): disable load config file

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -5,6 +5,7 @@
 
 provider "helm" {
   kubernetes {
+    load_config_file       = false
     host                   = var.host
     token                  = var.token
     cluster_ca_certificate = var.cluster_ca_certificate
