1344dae6c8d1162befacb6b9987ba1a00016d59f
Niels Hofmans <hello@ironpeak.be>
2020-02-25 12:13:18 +0100

    Update istio.tf

--- modules/helm/istio.tf
+++ modules/helm/istio.tf
@@ -7,4 +7,10 @@ resource "helm_release" "istio" {
   name        = "istio"
   repository  = data.helm_repository.istio.metadata[0].name
   chart       = "istio.io/istio"
+  version     = var.istio_version
+  namespace   = var.istio_namespace
+  
+  cleanup_on_fail = true
+  recreate_pods   = true
+  verify          = true
 }
