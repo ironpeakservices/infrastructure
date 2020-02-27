62e9176a8723aeb1845660be4d4677db32df535a
Niels Hofmans <hello@ironpeak.be>
2020-02-27 08:54:22 +0100

    Update main.tf

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
     load_config_file        = true
     config_path             = "kubeconfig"
-    config_context_cluster  = "ironpeakbe-main-cluster"
+    config_context_cluster  = var.cluster_name
     #cluster_ca_certificate  = base64decode(var.cluster_ca_certificate)
     #host    = var.host
     #token   = var.token
