356357afb9c617bbf27b8d6f53dc85e6389c16f3
Niels Hofmans <hello@ironpeak.be>
2020-02-27 09:14:56 +0100

    Update variables.tf

--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -7,7 +7,7 @@ variable cluster_ca_certificate {}
 variable cluster_name {}
 
 variable kubeconfig_path {
-  default = "${path.root}/kubeconfig"
+  default = "../../kubeconfig"
 }
 
 variable istio_namespace {
