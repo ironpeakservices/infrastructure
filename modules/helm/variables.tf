8db016b98c8372a24b9a722d1349b42ec6097284
Niels Hofmans <hello@ironpeak.be>
2020-02-27 08:55:17 +0100

    Update variables.tf

--- modules/helm/variables.tf
+++ modules/helm/variables.tf
@@ -1,13 +1,15 @@
-variable "host" {}
+variable host {}
 
-variable "token" {}
+variable token {}
 
-variable "cluster_ca_certificate" {}
+variable cluster_ca_certificate {}
 
-variable "istio_version" {
+variable cluster_name {}
+
+variable istio_version {
   default = "1.4.5"  
 }
 
-variable "istio_namespace" {
+variable istio_namespace {
   default = "system-istio"
 }
