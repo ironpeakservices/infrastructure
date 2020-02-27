3398352a86ce00fcf3fd7b41d6156623a2163fae
Niels Hofmans <hello@ironpeak.be>
2020-02-27 08:55:32 +0100

    Update main.tf

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -7,7 +7,7 @@ provider "helm" {
   kubernetes {
     load_config_file       = true
     config_path            = "kubeconfig"
-    config_context         = "ironpeakbe-main-cluster"
+    config_context         = var.cluster_name
     #host                   = var.host
     #token                  = var.token
     #cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
