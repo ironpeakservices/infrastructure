3d22fe218447ced77611076df74d63a5fb9596df
Niels Hofmans <hello@ironpeak.be>
2020-02-27 14:32:34 +0100

    Create loki.tf

---
+++ modules/helm/loki.tf
@@ -0,0 +1,19 @@
+# repo of the istio helm charts
+data "helm_repository" "loki" {
+  name = "loki"
+  url  = "https://grafana.github.io/loki/charts"
+}
+
+# install prometheus, 
+resource "helm_release" "istio_init" {
+  name        = "loki"
+  repository  = data.helm_repository.loki.metadata[0].name
+  chart       = "loki/loki-stack"
+  version     = var.loki_version
+  namespace   = var.loki_namespace
+  
+  #atomic          = true
+  #cleanup_on_fail = true
+  recreate_pods   = true
+  verify          = false
+}
