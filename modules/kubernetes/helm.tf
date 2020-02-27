6e1e2645e963c471b26276c91d7baadbe9dd9062
Niels Hofmans <hello@ironpeak.be>
2020-02-27 12:11:35 +0100

    Create helm.tf

---
+++ modules/kubernetes/helm.tf
@@ -0,0 +1,24 @@
+resource "kubernetes_service_account" "sa_system_tiller" {
+  metadata {
+    name = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "rb_system_tiller" {
+  metadata {
+    name = "tiller"
+  }
+  
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  
+  subject {
+    kind      = "ServiceAccount"
+    name      = "${kubernetes_service_account.sa_system_tiller.metadata.0.name}"
+    namespace = "kube-system"
+  }
+}
