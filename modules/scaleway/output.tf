82a283b0d84ad7c31a051f737af412c482b65282
Niels Hofmans <hello@ironpeak.be>
2020-02-27 09:31:23 +0100

    Update output.tf

--- modules/scaleway/output.tf
+++ modules/scaleway/output.tf
@@ -14,6 +14,21 @@ output "wildcard_dns" {
     value = scaleway_k8s_cluster_beta.ironpeakbe-main-cluster.wildcard_dns
 }
 
+output "host" {
+    sensitive = true
+    value     = scaleway_k8s_cluster_beta.ironpeakbe-main-cluster.kubeconfig[0].config_file.host
+}
+
+output "token" {
+    sensitive = true
+    value     = scaleway_k8s_cluster_beta.ironpeakbe-main-cluster.kubeconfig[0].config_file.token
+}
+
+output "cluster_ca_certificate" {
+    sensitive = true
+    value     = scaleway_k8s_cluster_beta.ironpeakbe-main-cluster.kubeconfig[0].cluster_ca_certificate
+}
+
 output "kubeconfig" {
     sensitive = true
     value     = scaleway_k8s_cluster_beta.ironpeakbe-main-cluster.kubeconfig
