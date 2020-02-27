49e24355c1551b94931b9485710412426a201dcb
Niels Hofmans <hello@ironpeak.be>
2020-02-27 11:26:31 +0100

    fix(helm): add tiller service account

--- modules/kubernetes/ns-istio.tf
+++ modules/kubernetes/ns-istio.tf
@@ -13,3 +13,28 @@ resource "kubernetes_namespace" "istio" {
         name = var.istio_namespace
     }
 }
+
+# have a service account to deploy via tiller (helm v2)
+resource "kubernetes_service_account" "sa_system_tiller" {
+    metadata {
+        namespace = "kube-system"
+        name = "tiller"
+    }
+}
+
+# now bind the role to the user for tiller (helm v2)
+resource "kubernetes_role_binding" "rb_system_tiller" {
+  metadata {
+    namespace = "kube-system"
+    name      = "rb-system-tiller"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.sa_system_tiller.metadata.0.name
+  }
+}
