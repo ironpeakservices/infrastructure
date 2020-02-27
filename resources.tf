62aaa99fd1bbcb993f2dd83a9ea24a9ce73cc3fd
Niels Hofmans <hello@ironpeak.be>
2020-02-27 09:43:36 +0100

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
-resource "local_file" "kubeconfig" {
-    file_permission = "0400"
-    filename = "kubeconfig"
-    sensitive_content  = module.scaleway.kubeconfig[0].config_file
-}
+#resource "local_file" "kubeconfig" {
+#    file_permission = "0400"
+#    filename = "kubeconfig"
+#    sensitive_content  = module.scaleway.kubeconfig[0].config_file
+#}
