165c015de36193d3719b5b7e6823fefe0acf82ad
Niels Hofmans <hello@ironpeak.be>
2020-02-28 10:03:09 +0100

    Update istio.tf

--- modules/helm/istio.tf
+++ modules/helm/istio.tf
@@ -19,9 +19,25 @@ resource "helm_release" "istio_init" {
   verify          = false
 }
 
+# ability to use istio as a CNI instead of cilium/canico
+resource "helm_release" "istio_cni" {
+  depends_on  = [helm_release.istio_init]
+
+  name        = "istio-cni"
+  repository  = data.helm_repository.istio.metadata[0].name
+  chart       = "istio-cni"
+  version     = var.istio_version
+  namespace   = var.istio_namespace
+  
+  #atomic          = true
+  #cleanup_on_fail = true
+  recreate_pods   = true
+  verify          = false
+}
+
 # istio chart for the real components
 resource "helm_release" "istio" {
-  depends_on  = [helm_release.istio_init]
+  depends_on  = [helm_release.istio_cni]
 
   name        = "istio"
   repository  = data.helm_repository.istio.metadata[0].name
@@ -51,23 +67,15 @@ resource "helm_release" "istio" {
     value = "true"
   }
   set { # enable the cni plugin to replace canico
-    name  = "cni.enabled"
+    name  = "istio_cni.enabled"
     value = "true"
   }
-}
-
-# ability to use istio as a CNI instead of cilium/canico
-resource "helm_release" "istio_cni" {
-  depends_on  = [helm_release.istio]
-
-  name        = "istio-cni"
-  repository  = data.helm_repository.istio.metadata[0].name
-  chart       = "istio-cni"
-  version     = var.istio_version
-  namespace   = var.istio_namespace
-  
-  #atomic          = true
-  #cleanup_on_fail = true
-  recreate_pods   = true
-  verify          = false
+  set { # enable container mtls by default
+    name  = "mtls.enabled"
+    value = "true"
+  }
+  set { # only allow traffic that is defined in the service registry
+    name  = "outboundTrafficPolicy.mode"
+    value = "REGISTRY_ONLY"
+  }
 }
