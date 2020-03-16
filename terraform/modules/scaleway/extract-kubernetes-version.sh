78e076212b7d3363507345ec725f8b22f79a172f
Niels Hofmans <hello@ironpeak.be>
2020-03-16 19:44:42 +0100

    fix(scaleway): +1 version

--- terraform/modules/scaleway/extract-kubernetes-version.sh
+++ terraform/modules/scaleway/extract-kubernetes-version.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-version="$(grep k8s "$1" | cut -d ' ' -f 3 | cut -d 'v' -f 2)"
+version="$(grep k8s "$1" | cut -d ' ' -f 3 | cut -d 'v' -f 2 | sed 's/^0/1/')"
 
 jq -n --arg version "$version" '{"version":$version}'
