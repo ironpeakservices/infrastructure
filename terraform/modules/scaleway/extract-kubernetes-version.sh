3b82232cc3519ad3c014b34dd01bd7884538b495
Niels Hofmans <hello@ironpeak.be>
2020-03-16 18:53:05 +0100

    Update extract-kubernetes-version.sh

--- terraform/modules/scaleway/extract-kubernetes-version.sh
+++ terraform/modules/scaleway/extract-kubernetes-version.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-eval "$(jq -r '@sh "FOO=\(.file)"')"
-
-version="$(cat "$file" | grep kubernetes | cut -d ' ' -f 3 | cut -d 'v' -f 2)"
+echo "$1" | grep kubernetes | cut -d ' ' -f 3 | cut -d 'v' -f 2)"
 
 jq -n --arg version "$version" '{"version":$version}'
