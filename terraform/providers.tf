7fb17c29952c8479e1f8c270a1ae1aa72ac657b7
Niels Hofmans <hello@ironpeak.be>
2020-08-20 13:36:05 +0200

    fix(terraform): use new providers format

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,42 +1,47 @@
 terraform {
-    required_version = ">= v0.13.0"
+    required_version = ">= v0.12.19"
     
     backend "local" {
         path = "state/terraform.tfstate"    
     }
-}
+    
+    required_providers {
+        external = {
+            version = "~> 1.2"
+        }
+        
+        local = {
+            version = "~> 1.4"
+        }
+        
+        template = {
+            version = "~> 2.1"
+        }
+        
+        cloudflare = {
+            source  = "cloudflare/cloudflare"
+            version = "~> 2.9"
 
-provider "external" {
-    version = "~> 1.2"
-}
+            api_token = var.cloudflare_token
+        }
+        
+        github = {
+            source  = "hashicorp/github"
+            version = "~> 2.9"
 
-provider "local" {
-    version = "~> 1.4"
-}
+            token = var.github_token
+            organization = var.github_org    
+        }
+        
+        scaleway = {
+            source  = "scaleway/scaleway"
+            version = "~> 1.16"
 
-provider "template" {
-    version = "~> 2.1"
-}
-
-provider "cloudflare" {
-    version = "~> 2.3"
-
-    api_token = var.cloudflare_token
-}
-
-provider "github" {
-    version = "~> 2.2"
-
-    token = var.github_token
-    organization = var.github_org
-}
-
-provider "scaleway/scaleway" {
-    version = "~> 1.14"
-
-    access_key      = var.scaleway_accesstoken
-    secret_key      = var.scaleway_secrettoken
-    organization_id = var.scaleway_org
-    zone            = var.scaleway_zone
-    region          = var.scaleway_region
+            access_key      = var.scaleway_accesstoken
+            secret_key      = var.scaleway_secrettoken
+            organization_id = var.scaleway_org
+            zone            = var.scaleway_zone
+            region          = var.scaleway_region
+        }
+    }
 }
